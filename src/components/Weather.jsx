013046c878ab79d16d32186980c72219d5d5dc63
Kelly <kellybytes.ca@gmail.com>
2025-10-10 22:10:29 -0600

    Update Weather section UI
    - Change layouts for mobile and desktop
    - Update the search field with animation
    - Make the search field close when clicking outside

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 
 const Weather = () => {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
+  const [showInput, setShowInput] = useState(false);
+  const [animating, setAnimating] = useState(false);
+  const inputRef = useRef(null);
 
   const fetchWeather = async (city) => {
     const W_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
@@ -38,6 +41,19 @@ const Weather = () => {
   const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
       search();
+      setShowInput(false);
+    }
+  };
+
+  const handleToggle = () => {
+    if (showInput) {
+      setAnimating(true);
+      setTimeout(() => {
+        setAnimating(false);
+        setShowInput(false);
+      }, 250);
+    } else {
+      setShowInput(true);
     }
   };
 
@@ -56,9 +72,9 @@ const Weather = () => {
       case 'Fog':
       case 'Haze':
       case 'Mist':
-        return 'bxs-cloud text-neutral-50';
+        return 'bxs-cloud text-neutral-400';
       default:
-        return 'bxs-cloud text-neutral-50';
+        return 'bxs-cloud text-neutral-400';
     }
   };
 
@@ -91,64 +107,139 @@ const Weather = () => {
     fetchWeather('Edmonton');
   }, []);
 
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (inputRef.current && !inputRef.current.contains(e.target)) {
+        setAnimating(true);
+        setTimeout(() => {
+          setAnimating(false);
+          setShowInput(false);
+        }, 250);
+      }
+    };
+
+    if (showInput) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [showInput]);
+
   return (
-    <div className="weather w-full lg:w-60 xl:w-72 2xl:w-94 h-full lg:h-[45%] flex flex-col justify-center items-center gap-y-4 xl:gap-y-6 2xl:gap-y-8 p-4 bg-zinc-900 rounded-xl">
-      <div className="search flex flex-col gap-y-2">
-        <div className="search-top flex">
-          <i className="fa-solid fa-location-dot text-base 2xl:text-lg text-neutral-200 translate-y-1 xl:translate-y-2"></i>
-          <div className="location font-comfortaa font-bold text-sm xl:text-lg 2xl:text-xl text-neutral-50 pl-2">
-            {data.name}
-            {data.sys ? `, ${data.sys.country}` : null}
-          </div>
-        </div>
-        <div className="search-location relative">
-          <input
-            type="text"
-            placeholder="Enter Location"
-            className="w-60 lg:w-48 xl:w-54 h-10 bg-transparent outline-0 border-b-1 border-[#aaa] xl:text-lg 2xl:text-xl text-neutral-200 pl-4"
-            value={location}
-            onFocus={(e) => (e.target.value = '')}
-            onChange={handleInputChange}
-            onKeyDown={handleKeyDown}
-          />
-          <i
-            className="fa-solid fa-magnifying-glass absolute top-0 translate-y-3 right-4 text-lg text-neutral-50/80"
-            onClick={search}
-          ></i>
-        </div>
+    <div className="weather w-full h-fit flex flex-row lg:flex-col justify-around lg:justify-center items-center gap-y-4 p-4 rounded-xl shadow-lg bg-neutral-50 text-neutral-900">
+      <div className="date font-comfortaa text-sm sm:text-base md:text-lg">
+        <p>
+          {new Date().toLocaleString('en-CA', {
+            weekday: 'short',
+            month: 'short',
+            day: '2-digit',
+            year: 'numeric',
+          })}
+        </p>
       </div>
+
       {data.notFound ? (
-        <div className="font-comfortaa text-lg xl:text-xl text-neutral-200 mb-12">
+        <div className="font-comfortaa font-bold text-lg xl:text-xl mb-12">
           Not Found 😔
         </div>
       ) : (
-        <div className="weather-data flex flex-col items-center">
+        <div className="weather-data flex flex-row items-center gap-x-3 lg:flex-col gap-y-1">
+          <div className="location hidden lg:flex justify-center gap-x-3">
+            <div className="flex">
+              <i className="fa-solid fa-location-dot xl:text-lg translate-y-1 xl:translate-y-2"></i>
+              <div className="font-comfortaa font-bold xl:text-lg 2xl:text-xl pl-2">
+                {data.name}
+                {data.sys ? `, ${data.sys.country}` : null}
+              </div>
+            </div>
+
+            <div className="search-location relative">
+              <i
+                className="fa-solid fa-magnifying-glass xl:text-lg  xl:translate-y-1 text-neutral-900/80 cursor-pointer"
+                onClick={handleToggle}
+              ></i>
+              {showInput && (
+                <div
+                  ref={inputRef}
+                  className={`search-input absolute right-6 top-1/2 -translate-y-1/2 bg-white/90 backdrop-blur-md shadow-lg rounded-xl p-2 z-10 ${
+                    animating ? 'animate-fadeOutX' : 'animate-fadeInX'
+                  } `}
+                >
+                  <input
+                    type="text"
+                    placeholder="Enter Location"
+                    className="w-52 h-10 bg-transparent outline-0 xl:text-lg 2xl:text-xl pl-4"
+                    value={location}
+                    // onFocus={(e) => (e.target.value = '')}
+                    autoFocus
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                  />
+                </div>
+              )}
+            </div>
+          </div>
+
           <i
             className={`bx ${
               data.weather &&
               data.weather[0] &&
               getWeatherIcon(data.weather[0].main)
-            } text-4xl xl:text-6xl 2xl:text-7xl`}
+            } text-[clamp(2.25rem,1.8rem+2.35vw,4.5rem)]`}
           ></i>
-          <div className="weather-type font-comfortaa text-sm xl:text-lg 2xl:text-xl text-neutral-200 mb-2">
+
+          <div className="weather-type hidden lg:block font-comfortaa text-[clamp(0.875rem,0.8rem+0.475vw,1.25rem)]">
             {data.weather ? data.weather[0].main : null}
           </div>
-          <div className="temp font-comfortaa text-sm xl:text-xl 2xl:text-2xl text-neutral-200 mb-2">
-            {data.main ? `${Math.floor(data.main.temp)}℃` : null}
+
+          <div className="flex flex-col">
+            <div className="lg:hidden font-comfortaa text-xs sm:text-sm md:text-base">
+              {data.name}
+            </div>
+            <div className="temp font-comfortaa font-bold text-xl xl:text-2xl lg:pb-2">
+              {data.main ? `${Math.floor(data.main.temp)}℃` : null}
+            </div>
           </div>
           {/* For max/min temperatures
-        <div className="temp-minmax font-comfortaa text-[1.4rem] text-neutral-400">
-          {data.main ? (
-            <>
-              <i className="fa-solid fa-arrow-up text-[1.2rem]" />{' '}
-              {Math.floor(data.main.temp_max)}℃ /{' '}
-              <i className="fa-solid fa-arrow-down text-[1.2rem]" />{' '}
-              {Math.floor(data.main.temp_min)}℃
-            </>
-          ) : null}
-        </div> */}
-          <div className="extra-info flex gap-6 xl:mt-4">
-            <div className="humidity text-xs xl:text-base 2xl:text-lg text-neutral-400">
+          <div className="temp-minmax font-comfortaa text-[1.4rem] text-neutral-400">
+            {data.main ? (
+              <>
+                <i className="fa-solid fa-arrow-up text-[1.2rem]" />{' '}
+                {Math.floor(data.main.temp_max)}℃ /{' '}
+                <i className="fa-solid fa-arrow-down text-[1.2rem]" />{' '}
+                {Math.floor(data.main.temp_min)}℃
+              </>
+            ) : null}
+          </div>  */}
+
+          <div className="search-location lg:hidden relative">
+            <i
+              className="fa-solid fa-magnifying-glass xl:text-lg  xl:translate-y-1 text-neutral-900/80 cursor-pointer"
+              onClick={handleToggle}
+            ></i>
+            {showInput && (
+              <div
+                ref={inputRef}
+                className={`search-input absolute right-6 top-1/2 -translate-y-1/2 bg-white/90 backdrop-blur-md shadow-lg rounded-xl p-2 z-10 ${
+                  animating ? 'animate-fadeOutX' : 'animate-fadeInX'
+                } `}
+              >
+                <input
+                  type="text"
+                  placeholder="Enter Location"
+                  className="w-52 h-10 bg-transparent outline-0 xl:text-lg 2xl:text-xl pl-4"
+                  value={location}
+                  autoFocus
+                  onChange={handleInputChange}
+                  onKeyDown={handleKeyDown}
+                />
+              </div>
+            )}
+          </div>
+          <div className="extra-info hidden lg:flex gap-6">
+            <div className="humidity text-sm xl:text-base 2xl:text-lg text-neutral-700">
               {data.main ? (
                 <>
                   <i className="fa-solid fa-droplet"></i> {data.main.humidity}
@@ -156,7 +247,7 @@ const Weather = () => {
                 </>
               ) : null}
             </div>
-            <div className="wind text-xs xl:text-base 2xl:text-lg text-neutral-400">
+            <div className="wind text-sm xl:text-base 2xl:text-lg text-neutral-700">
               {data.wind ? (
                 <>
                   <i className="fa-solid fa-wind"></i>{' '}
